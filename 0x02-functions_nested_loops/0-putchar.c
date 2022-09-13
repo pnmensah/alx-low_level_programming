631b8cc9b2eb499a2b182852aa11d6670b4625d9
‘pnmensah’ <pnmensah@live.com>
2022-09-13 12:09:25 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+//**
+// * main - Entry point
+// *
+// * Return: Always 0 (Success)
+// */
+
+int main(void)
+{
+char c[8] = "_putchar";
+int i;
+for (i = 0; i < 8; i++)
+{
+putchar(c[i]);
+}
+putchar('\n');
+return (0);
+}
