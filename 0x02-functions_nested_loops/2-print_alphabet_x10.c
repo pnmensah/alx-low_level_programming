ea4774e996a1de00a0a352814b3177b1c1b2dd3e
‘pnmensah’ <pnmensah@live.com>
2022-09-13 13:12:02 -0700

    2-print

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints 10 times the alphabet, 
- * in lowercase, followed by a new line.
- *
- * Return: Always 0 (Success)
- */
+* print_alphabet_x10 - prints 10 times the alphabet
+* in lowercase, followed by a new line.
+* Return: Always 0 (Success)
+*/
 
 void print_alphabet_x10(void)
 {
