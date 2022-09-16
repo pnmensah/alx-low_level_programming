d848d3fa3378468d5b2ad6502075bdd7d1c6eb44
‘pnmensah’ <pnmensah@live.com>
2022-09-15 18:39:53 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,36 +1,16 @@
 #include "main.h"
 
 /**
-* print_remaining_days - takes a date and prints how many days are
-* left in the year, taking leap years into account
-* @month: month in number format
-* @day: day of month
-* @year: year
-* Return: void
+*_isupper - checks if a  character is uppercase or not
+*@c: character to be tested
+*Return: 1 whether it is, 0 otherwise
 */
 
-void print_remaining_days(int month, int day, int year)
+int _isupper(int c)
 {
-if ((year % 400 == 0) || (year % 4 == 0 && year % 100 !=  0))
+if ((c >= 'A') && (c <= 'Z'))
 {
-if (month >= 3 && day >= 60)
-{
-day++;
+return (1);
 }
-
-printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 366 - day);
-}
-else
-{
-if (month == 2 && day == 60)
-{
-printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-}
-else
-{
-printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 365 - day);
-}
-}
+return (0);
 }
