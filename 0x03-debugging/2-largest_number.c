4900dc10b5fee0d97e6c78280bf1c63bc3895724
‘pnmensah’ <pnmensah@live.com>
2022-09-15 18:13:00 -0700

    2-largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,13 +10,13 @@
  */
 
 int largest_number(int a, int b, int c)
-{
-int largest;
-if (a >= b && a >= c)
+{int largest;
+
+if (a > b && a > c)
 {
 largest = a;
 }
-else if (b >= a && b >= c)
+else if (b > c)
 {
 largest = b;
 }
@@ -24,5 +24,6 @@ else
 {
 largest = c;
 }
+
 return (largest);
 }
