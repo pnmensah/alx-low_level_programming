49d1b1ebcaed32abadcda23cde8f262909785fa7
‘pnmensah’ <pnmensah@live.com>
2022-09-22 15:17:38 -0700

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,7 +2,7 @@
 
 /**
  *_strcat - concatenates  the string pointed to by @src to
- * the end of the string pointed to by @dest
+ *the end of the string pointed to by @dest
  *@dest: String that will be appended
  *@src: String to be concatenated upon
  *
