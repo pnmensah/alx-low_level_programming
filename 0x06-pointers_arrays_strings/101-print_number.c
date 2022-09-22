dbbe677b5e16d67573f1d72aea79166ddbbf8c5e
‘pnmensah’ <pnmensah@live.com>
2022-09-22 15:27:06 -0700

    101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,21 +3,20 @@
 /**
  * print_number - prints an integer
  * @n:.input integer parameter
- *
- *
  */
+
 void print_number(int n)
 {
-	unsigned int i = n;
+unsigned int i = n;
 
-	if (n < 0)
-	{
-		_putchar(45);
-		i = -i;
-	}
-	if (i / 10)
-	{
-		print_number(i / 10);
-	}
-	_putchar(i % 10 + '0');
+if (n < 0)
+{
+_putchar(45);
+i = -i;
+}
+if (i / 10)
+{
+print_number(i / 10);
+}
+_putchar(i % 10 + '0');
 }
