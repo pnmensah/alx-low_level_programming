6a462976fcb0d605ab19079b5bb9ed14cdd1f573
‘pnmensah’ <pnmensah@live.com>
2022-09-28 10:28:47 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,6 +9,7 @@
 int is_prime_number(int n)
 {
 	int start = n / 2;
+
 	if (n <= 1)
 		return (0);
 	return (is_prime(n, start));
@@ -27,6 +28,6 @@ int is_prime(int n, int start)
 		return (1);
 	else if (n % start == 0)
 		return (0);
-	return (is_prime(n, start -1));
+	return (is_prime(n, start - 1));
 }
 
