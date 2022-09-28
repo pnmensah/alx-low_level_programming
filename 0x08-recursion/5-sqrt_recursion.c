28b73cfd884fbda79f32b0c8ffb651a1f1f5db51
‘pnmensah’ <pnmensah@live.com>
2022-09-28 10:11:03 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,9 +10,8 @@ int _sqrt_recursion(int n)
 {
 	if (n == 1 || n== 0)
 		return (n);
-	return (_sqrt(o, n));
+	return (_sqrt(0, n));
 }
-
 /**
  * _sqrt - returns the square root of a number
  * @n: test number
@@ -25,7 +24,7 @@ int _sqrt(int n, int x)
 	if (n > x / 2)
 		return (-1);
 	else if (n * n == x)
-		return (n):
+		return (n);
 			return (_sqrt(n + 1, x));
 }
 
