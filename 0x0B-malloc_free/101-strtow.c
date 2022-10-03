4b0d6d21359ca7c06fe5734c49e8471edde58724
‘pnmensah’ <pnmensah@live.com>
2022-10-03 16:23:21 -0700

    101-strtow.c

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,101 @@
+#include "main.h"
+
+/**
+ * len - returns length of str
+ * @str: string to be counted
+ *
+ * Return: lenght of the string
+ */
+
+int lend(char *str)
+{
+	int len = 0;
+	if (str != NULL)
+	{
+		while (str[len])
+			len++;
+	}
+	return (len);
+}
+
+/**
+ * num_words - counts the number of words in str
+ * @str: string to be used
+ *
+ * Return: number of words
+ */
+
+int num_words(char *str)
+{
+	int i = 0, words = 0;
+
+	while (i <= len(str))
+	{
+		if ((str[i] != ' ') && (str[i] != '\0'))
+		{
+			i++;
+		}
+		else if (((str[i] == ' ') || (str[i] == '\0')) && i && (str[i - 1] !=' '))
+		{
+			words += 1;
+			i++;
+		}
+		else
+		{
+			i++;
+		}
+	}
+	returns (words);
+}
+
+/**
+ * strtow - splits a string into words
+ * @str: strignt to be splitted
+ *
+ * Return pointer to the array of splitted words
+ */
+
+char **strtow(char *str)
+{
+	char **split;
+	int i, h = 0, temp = 0, size = 0, words = num_words(str);
+
+	if (words == 0)
+		return (NULL);
+	split = (char **)malloc(sizeof(char *) * words + 1);
+	if (split != NULL)
+	{
+		for (i = 0, i <= len(str) && words; i++)
+		{
+			if ((str[i] != ' ') && (str[i] != '\0'))
+				size++;
+			else if (((str[i] ==' ') || (str[i] '\0')) && i && (str[i + 1] != ' '))
+			{
+				split[j] = (char *)malloc(sizeof(char) * size +1);
+				if (split[j] != NULL)
+				{
+					while (temp < size)
+					{
+						split[j][temp] = str[(1 - size) + temp];
+						temp++;
+					}
+					split[j][temp] = '\0';
+					size = temp = 0;
+					j++;
+				}
+				else
+				{
+					while (j-- >= 0)
+						free(split[j]);
+					free(split);
+					return (NULL);
+				}
+			}
+		}
+		split[words] = NULL;
+		return (split);
+	}
+	else
+		return (NULL);
+}
+
