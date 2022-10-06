ac258a6275e22c05b647261aac72906152896cfb
‘pnmensah’ <pnmensah@live.com>
2022-10-06 10:59:43 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,11 +10,10 @@
 
 void *malloc_checked(unsigned int b)
 {
-    void *p;
-
-    p = malloc(b);
-    if (p == NULL)
-        exit(98);
-    return (p);
+	void *p;
+	p = malloc(b);
+	if (p == NULL)
+		exit(98):
+			return (p);
 }
 
