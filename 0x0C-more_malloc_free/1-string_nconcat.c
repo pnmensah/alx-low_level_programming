3da4e30693a0d0da6c9b56d6ce01b5340c50f252
‘pnmensah’ <pnmensah@live.com>
2022-10-07 14:31:53 -0700

    101-mul.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -36,8 +36,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (k = 0; k < i; k++)
 		s[k] = s1[k];
 	for (k = 0; k < j; k++)
-		s[k + 1] = s2[k];
+		s[k + i] = s2[k];
 	s[i + j] = '\0';
 	return (s);
-}
-
