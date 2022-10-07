354cfa3a3dfaea3effb706a5b84fd5f34a72edb6
‘pnmensah’ <pnmensah@live.com>
2022-10-07 00:10:48 -0700

    3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,17 +11,17 @@
 
 int *array_range(int min, int max)
 {
-    int *p;
-    int i, size;
+	int *p;
+	int i, size;
 
-    if (min > max)
-        return (NULL);
-    size = max - min + 1;
-    p = malloc(sizeof(int) * size);
-    if (p == NULL)
-        return (NULL);
-    for (i = 0; i < size; i++)
-        p[i] = min + i;
-    return (p);
+	if (min > max)
+		return (NULL);
+	size = max - min + 1;
+	p = malloc(sizeof(int) * size);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		p[i] = min + i;
+	return (p);
 }
 
