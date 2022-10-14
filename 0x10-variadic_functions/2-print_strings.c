107ca1280551af4e1379897e8711db5aaf23c4d4
‘pnmensah’ <pnmensah@live.com>
2022-10-13 18:13:37 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,7 +24,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		else
 			printf("(nil)");
 		if (i < n - 1 && separator)
-			printf("%s", separator)
+			printf("%s", separator);
 	}
 	printf("\n");
 	va_end(valist);
