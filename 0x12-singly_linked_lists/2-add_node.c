571ad4e8b3fb405d75d51a086e640175bc2af760
‘pnmensah’ <pnmensah@live.com>
2022-10-20 22:04:22 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,29 +12,29 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-        char *dup;
-        int len;
-        list_t *new;
+	char *dup;
+	int len;
+	list_t *new;
 
-        new = malloc(sizeof(list_t));
-        if (new == NULL)
-                return (NULL);
+	new = malloc(sizeof(list_t));
 
-        dup = strdup(str);
-        if (dup == NULL)
-        {
-                free(new);
-                return (NULL);
-        }
+	if (new == NULL)
+		return (NULL);
 
-        for (len = 0; str[len];)
-                len++;
+	dup = strdup(str);
 
-        new->str = dup;
-        new->len = len;
-        new->next = *head;
+	if (dup == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
 
-        *head = new;
+	for (len = 0; str[len];)
+		len++;
 
-        return (new);
+	new->str = dup;
+	new->len = len;
+	new->next = *head;
+	*head = new;
+	return (new);
 }
